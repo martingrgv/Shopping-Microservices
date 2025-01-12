809d73e321ec99a27b879d22d7a1af4e47d00d80
martingrgv <martin.geor04@gmail.com>
2025-01-12 23:59:20 +0200

    Added Products feature

---
+++ src/Services/Catalog/Catalog.API/Models/Product.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Catalog.API.Models
+{
+    public class Product
+    {
+        [Key]
+        public Guid Id { get; set; }
+        public string Name { get; set; } = default!;
+        public List<string> Category { get; set; } = new();
+        public string Description { get; set; } = default!;
+        public string ImageFile { get; set; } = default!;
+        public decimal Price { get; set; }
+    }
+}
