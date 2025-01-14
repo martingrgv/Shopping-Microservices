6f42fe487a92a9198534c0734b757898fa1fdf52
martinwlsngrgv <martin.geor04@gmail.com>
2025-01-15 01:06:50 +0200

    Added delete endpoint

---
+++ src/Services/Catalog/Catalog.API/Products/DeleteProduct/DeleteProductHandler.cs
@@ -0,0 +1,19 @@
+namespace Catalog.API.Products.DeleteProduct
+{
+    public record DeleteProductCommand(Guid Id) : ICommand<DeleteProductResult>;
+    public record DeleteProductResult(bool Success);
+
+    public class DeleteProductHandler(IDocumentSession session, ILogger<DeleteProductHandler> logger)
+        : ICommandHandler<DeleteProductCommand, DeleteProductResult>
+    {
+        public async Task<DeleteProductResult> Handle(DeleteProductCommand command, CancellationToken cancellationToken)
+        {
+            logger.LogInformation($"{nameof(DeleteProductHandler)}.{nameof(Handle)} called with {command}");
+
+            session.Delete<Product>(command.Id);
+            await session.SaveChangesAsync(cancellationToken);
+
+            return new DeleteProductResult(true);
+        }
+    }
+}
