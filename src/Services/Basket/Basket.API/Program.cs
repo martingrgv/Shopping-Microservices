23d301ec0268bead21bf137dcf76723c21c7a7b4
martinwlsngrgv <martin.geor04@gmail.com>
2025-01-27 02:39:53 +0200

    Added marten library, connection string and configuration, implemented repository pattern, created basket not found exception

--- src/Services/Basket/Basket.API/Program.cs
+++ src/Services/Basket/Basket.API/Program.cs
@@ -1,3 +1,5 @@
+using Marten;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddCarter();
@@ -8,6 +10,13 @@ builder.Services.AddMediatR(configuration =>
     configuration.AddOpenBehavior(typeof(ValidationBehavior<,>));
     configuration.AddOpenBehavior(typeof(LoggingBehavior<,>));
 });
+
+builder.Services.AddMarten(options =>
+{
+    options.Connection(builder.Configuration.GetConnectionString("BasketDb")!);
+    options.Schema.For<ShoppingCart>().Identity(x => x.UserName);
+    options.DisableNpgsqlLogging = true;
+}).UseLightweightSessions();
 
 
 var app = builder.Build();
