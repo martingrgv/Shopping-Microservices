ff9423206bab25f0d292abe05a1dff9455f81e33
martinwlsngrgv <martin.geor04@gmail.com>
2025-02-10 19:34:34 +0200

    Fixed database, implemented get discount method

--- src/Services/Discount/Discount.Grpc/Data/DiscountDbContext.cs
+++ src/Services/Discount/Discount.Grpc/Data/DiscountDbContext.cs
@@ -6,11 +6,12 @@ namespace Discount.Grpc.Data;
 public class DiscountDbContext(DbContextOptions<DiscountDbContext> options) 
     : DbContext(options)
 {
-    public DbSet<Coupon> Discounts { get; set; } = default!;
-
+    public DbSet<Coupon> Coupons { get; set; } = default!;
+    
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
-        modelBuilder.Entity<CouponModel>()
+        modelBuilder.Entity<Coupon>().HasKey(x => x.Id);
+        modelBuilder.Entity<Coupon>()
             .HasData(
                 new Coupon{ Id = 1, ProductName = "Macbook 14 Pro", Description =  "Apple products discount", Amount = 5 },
                 new Coupon{ Id = 2, ProductName = "Lenovo IdeaPad 3", Description = "PC discount", Amount = 15 });
--- src/Services/Discount/Discount.Grpc/Services/DiscountService.cs
+++ src/Services/Discount/Discount.Grpc/Services/DiscountService.cs
@@ -1,13 +1,33 @@
+using Discount.Grpc.Data;
+using Discount.Grpc.Models;
 using Grpc.Core;
+using Mapster;
+using Microsoft.EntityFrameworkCore;
 
 namespace Discount.Grpc.Services;
 
-public class DiscountService : DiscountProtoService.DiscountProtoServiceBase
+public class DiscountService(DiscountDbContext dbContext, ILogger<DiscountService> logger)
+    : DiscountProtoService.DiscountProtoServiceBase
 {
-    public override Task<CouponModel> GetDiscount(GetDiscountRequest request, ServerCallContext context)
+    public override async Task<CouponModel> GetDiscount(GetDiscountRequest request, ServerCallContext context)
     {
-        // TODO: GetDiscount from Database
-        return base.GetDiscount(request, context);
+        var coupon = await dbContext
+            .Coupons
+            .FirstOrDefaultAsync(c => c.ProductName == request.ProductName);
+
+        if (coupon is null)
+        {
+            coupon = new Coupon
+            {
+                ProductName = "No Discount",
+                Amount = 0
+            };
+        }
+        
+        logger.LogInformation("Discount retrieved for ProductName : {productName}, Amount : {amount}", coupon.ProductName, coupon.Amount);
+
+        var couponModel = coupon.Adapt<CouponModel>();
+        return couponModel;
     }
 
     public override Task<CouponModel> CreateDiscount(CreateDiscountRequest request, ServerCallContext context)
