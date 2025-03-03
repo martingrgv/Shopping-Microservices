5761ed3da9eae671cb26b307e8a7bbc070ac568a
martinwlsngrgv <martin.geor04@gmail.com>
2025-03-03 23:01:06 +0200

    Added feature management for publishing domain event

--- src/Services/Ordering/Ordering.Application/Orders/EventHandlers/Domain/OrderCreatedEventHandler.cs
+++ src/Services/Ordering/Ordering.Application/Orders/EventHandlers/Domain/OrderCreatedEventHandler.cs
@@ -1,17 +1,20 @@
 using MassTransit;
 using Microsoft.Extensions.Logging;
+using Microsoft.FeatureManagement;
 
 namespace Ordering.Application.Orders.EventHandlers.Domain;
 
-public class OrderCreatedEventHandler(ILogger<OrderCreatedEventHandler> logger, IPublishEndpoint publishEndpoint)
+public class OrderCreatedEventHandler(ILogger<OrderCreatedEventHandler> logger, IPublishEndpoint publishEndpoint, IFeatureManager featureManager)
     : INotificationHandler<OrderCreatedEvent>
 {
     public async Task Handle(OrderCreatedEvent domainEvent, CancellationToken cancellationToken)
     {
         logger.LogInformation("Domain Event handled: {DomainEvent}", domainEvent.GetType());
 
-        var orderCreatedIntegrationEvent = domainEvent.order.ToOrderDto();
-        
-        await publishEndpoint.Publish(orderCreatedIntegrationEvent, cancellationToken);
+        if (await featureManager.IsEnabledAsync("OrderFulfillment"))
+        {
+            var orderCreatedIntegrationEvent = domainEvent.order.ToOrderDto();
+            await publishEndpoint.Publish(orderCreatedIntegrationEvent, cancellationToken);
+        }
     }
 }
