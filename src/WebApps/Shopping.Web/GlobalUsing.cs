a13f4deb8db6ebe3c2fb3d88f990a00de48492b3
martin.grgv <martin.geor04@gmail.com>
2025-03-24 01:00:33 +0200

    Added IOrderService.cs

--- src/WebApps/Shopping.Web/GlobalUsing.cs
+++ src/WebApps/Shopping.Web/GlobalUsing.cs
@@ -2,3 +2,4 @@ global using Refit;
 global using Shopping.Web.Contracts;
 global using Shopping.Web.Models.Catalog.Endpoint;
 global using Shopping.Web.Models.Basket.Endpoint;
+global using Shopping.Web.Models.Ordering.Endpoint;
